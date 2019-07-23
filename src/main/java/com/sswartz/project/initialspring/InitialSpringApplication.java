a7390643383fedc6a3f4a8e2de5f1fc2cf0d8d83
Stuart Swartz <stuart.swartz@parivedasolutions.com>
2019-07-23 10:20:29 -0500

    Added initial spring project

---
+++ src/main/java/com/sswartz/project/initialspring/InitialSpringApplication.java
@@ -0,0 +1,13 @@
+package com.sswartz.project.initialspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitialSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitialSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sswartz/project/initialspring/InitialSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sswartz.project.initialspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitialSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
